e0d26ecfa5341cb3f19d1d25278cd4c5ca36e3de
Adilbek <mv.adilbek@gmail.com>
2018-07-02 23:44:54 +0600

    First

---
+++ app/src/main/java/Controllers/ConnectExmpl.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+import java.sql.SQLException;
+
+/**
+ * Created by adilbekmailanov on 02.07.2018.
+ */
+
+public class ConnectExmpl {
+  public static void main(String[] args) throws SQLException {
+
+  }
+}
